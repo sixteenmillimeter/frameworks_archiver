18792b10348243513339778ff221912698371bcd
Matt <sixteenmillimeter@users.noreply.github.com>
2018-06-29 15:06:37 -0400

    Only use curl. Works on linux and mac

--- archive.sh
+++ archive.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-curl https://mailman-mail5.webfaction.com/pipermail/frameworks/ | grep "Gzip'd" > all.txt
+curl index.html https://mailman-mail5.webfaction.com/pipermail/frameworks/ | grep "Gzip'd" > all.txt
 
 cat all.txt | pup 'a attr{href}' > clean.txt
 
 awk '{print "https://mailman-mail5.webfaction.com/pipermail/frameworks/"$0}' clean.txt > links.txt
 
-rm all.txt clean.txt
+rm all.txt clean.txt index.html
 
 while read p; do
-  wget $p
+  curl -O $p
   sleep 5 #to be nice
 done < links.txt
